7a3fe689fdd35c5afa3c290b753270eac196fbc5
Ramon Noleto <devnoleto@gmail.com>
2024-11-06 17:00:13 -0300

    :wrench: feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 
-const LoginPage = () => {
+const LoginPage = async () => {
+    const { userId } = await auth();
+    if(userId){
+        redirect("/");
+    }
     return ( 
         <div className="grid h-full grid-cols-2">
             {/* ESQUERDA */}
@@ -13,7 +20,9 @@ const LoginPage = () => {
             <p className="text-muted-foreground mb-8">
                 A Finance AI é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, e oferecer insights personalizados, facilitando o controle do seu orçamento.
             </p>
-            <Button variant={"outline"}><LogInIcon className="mr-2"/> Fazer login ou criar conta</Button>
+            <SignInButton>
+                <Button variant={"outline"}><LogInIcon className="mr-2"/> Fazer login ou criar conta</Button>
+            </SignInButton>
             </div>
             {/* DIREITA */}
             <div className="relative h-full w-full">
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { dark } from "@clerk/themes";
+import { redirect } from "next/navigation";
 
-const Home = () => {
+const Home =  async () => {
+  const {userId} = await auth()
+  if(!userId){
+    redirect("/login");
+  }
   return(
-    <Button>Hello Word</Button>
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName appearance={{
+        baseTheme: dark
+      }} />
+    </div>
   )
 }
 
