52946cded8d12d5de2dad6ffda0add36506a70c1
Ramon Noleto <ramon.zap@hotmail.com>
2024-11-10 15:37:11 -0300

    :wrench: feat: ensure transactions and subscripions page are protected

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,6 +1,13 @@
-import Navbar from "../_components/navbar";
 
-const SubscriptionPage = () => {
+import { redirect } from "next/navigation";
+import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+
+const SubscriptionPage = async() => {
+   const {userId} = await auth();
+   if(!userId) {
+    redirect("/login");
+   }
     return <Navbar />
 };
 
