f21eb92e45b7239da52a0adb1ba85e684dec207c
Ramon Noleto <ramon.zap@hotmail.com>
2024-11-10 12:25:20 -0300

    :wrench: bkp: Config Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="en">
       <body
         className={`${mulish.className} antialiased dark`}
       >
