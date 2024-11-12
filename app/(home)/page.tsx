8d117d7cb6802fcf038962b3691cba731551b8af
Ramon Noleto <devnoleto@gmail.com>
2024-11-12 09:43:31 -0300

    :wrench: feat: ajustes de estilização

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -31,15 +31,15 @@ const Home = async ({ searchParams: { month } }) => {
   return (
     <>
       <Navbar />
-      <div className="p-6 space-y-6">
+      <div className="p-6 space-y-6 flex flex-col overflow-hidden">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        <div className="grid grid-cols-[2fr,1fr] gap-6">
-          <div className="flex flex-col gap-6">
+        <div className="grid grid-cols-[2fr,1fr] gap-6 overflow-hidden">
+          <div className="flex flex-col gap-6 overflow-hidden">
             <SummaryCards month={month} {...dashboard} />
-            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+            <div className="grid grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
               <TransactionPieChart {...dashboard} />
               <ExpensesPerCategory expensesPerCategory={dashboard.totalExpensePerCategory} />
             </div>
