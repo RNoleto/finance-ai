eae96adc90eb861c6cd2cbaca3346a2d3f2d125c
Ramon Noleto <devnoleto@gmail.com>
2024-11-13 08:40:07 -0300

    :wrench: feat: add ai report generation

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,6 +9,7 @@ import { getDashboad } from "../_data/get-dashboard";
 import ExpensesPerCategory from "./_components/expenses-per-category";
 import LastTransactions from "./_components/last-transactions";
 import { canUserAddTransaction } from "../_data/can-user-add-transaction";
+import AiReportButton from "./_components/ai-report-button";
 
 interface HomeProps {
   searchParams: {
@@ -36,7 +37,11 @@ const Home = async ({ searchParams: { month } }) => {
       <div className="p-6 space-y-6 flex flex-col overflow-hidden">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
+          {/* Botão IA */}
+          <div className="flex items-center gap-3">
+          <AiReportButton month={month}/>
           <TimeSelect />
+          </div>
         </div>
         <div className="grid grid-cols-[2fr,1fr] gap-6 overflow-hidden">
           <div className="flex flex-col gap-6 overflow-hidden">
